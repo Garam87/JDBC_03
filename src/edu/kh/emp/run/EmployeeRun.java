57ac18ef84e58ee27a949edec1bd1abacb2771e2
garam <dheotjd87@gmail.com>
2023-02-01 15:15:57 +0900

    1

---
+++ src/edu/kh/emp/run/EmployeeRun.java
@@ -0,0 +1,14 @@
+package edu.kh.emp.run;
+
+import edu.kh.emp.view.EmployeeView;
+
+public class EmployeeRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EmployeeView view = new EmployeeView();
+		view.displayMenu();
+	}
+
+}
